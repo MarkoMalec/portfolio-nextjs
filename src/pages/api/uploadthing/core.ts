723e04cbbfa634c92c70852f65ed7fa9a9e5eb7c
Marko Malec <cropsychooo@yahoo.com>
2023-08-16 16:59:22 +0200

    .

--- src/pages/api/uploadthing/core.ts
+++ src/pages/api/uploadthing/core.ts
@@ -1,18 +1,32 @@
-import { getToken } from 'next-auth/jwt'
-import { createUploadthing, type FileRouter } from 'uploadthing/next'
-
-const f = createUploadthing()
-
+import type { NextApiRequest, NextApiResponse } from "next";
+ 
+import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
+ 
+const f = createUploadthing();
+ 
+const auth = (req: NextApiRequest, res: NextApiResponse) => ({ id: "fakeId" }); // Fake auth function
+ 
+// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: '4MB' } })
-    .middleware(async (req) => {
-      const user = await getToken({ req })
-
-      if (!user) throw new Error('Unauthorized')
-
-      return { userId: user.id }
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req, res }) => {
+      // This code runs on your server before upload
+      const user = await auth(req, res);
+ 
+      // If you throw, the user will not be able to upload
+      if (!user) throw new Error("Unauthorized");
+ 
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
     })
-    .onUploadComplete(async ({ metadata, file }) => {}),
-} satisfies FileRouter
-
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+ 
+      console.log("file url", file.url);
+    }),
+} satisfies FileRouter;
+ 
 export type OurFileRouter = typeof ourFileRouter;
