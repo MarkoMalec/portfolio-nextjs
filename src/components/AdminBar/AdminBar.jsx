63f07de0d7cf874c353da09d3730805a959453fc
Marko Malec <CroPsychooo@yahoo.com>
2023-08-25 13:53:43 +0200

    .

--- src/components/AdminBar/AdminBar.jsx
+++ src/components/AdminBar/AdminBar.jsx
@@ -1,11 +1,7 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { useSession } from "next-auth/react";
 
-interface AdminBarProps {
-  children: ReactNode;
-}
-
-const AdminBar: React.FC<AdminBarProps> = ({ children }) => {
+const AdminBar = ({ children }) => {
   const { data: session } = useSession();
   if (session) {
     return <div id="adminbar">{children}</div>;
