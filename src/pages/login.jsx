aa9ee4b23ac26ee317810e053b7c139e385f7175
Marko Malec <CroPsychooo@yahoo.com>
2023-08-30 20:06:53 +0200

    .

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import SmallLogo from "@assets/SmallLogo.jsx";
 
 const Login = () => {
   const { data: session } = useSession();
@@ -13,23 +14,24 @@ const Login = () => {
   }, [session]);
 
   return (
-    <section className="login-section">
-      <div className="login_card">
-        <h3>Login with preferable service</h3>
-        <p>Currently only available one is Discord</p>
-        <div className="login-button-area">
-          <button className="btn-login" onClick={() => signIn("discord")}>
-            Sign in with Discord
-          </button>
-          <button className="btn-login" onClick={() => signIn("discord")}>
-            Sign in with Discord
-          </button>
-          <button className="btn-login" onClick={() => signIn("discord")}>
-            Sign in with Discord
-          </button>
+    <>
+      <section className="login-section">
+        <div className="login-logo">
+          <SmallLogo />
         </div>
-      </div>
-    </section>
+        <div className="login_card">
+          <h3>Login to dashboard</h3>
+          <div className="login-button-area">
+            <button className="btn-login google" onClick={() => signIn("google")}>
+              Sign in with <span>G</span><span>o</span><span>o</span><span>g</span><span>l</span><span>e</span>
+            </button>
+            <button className="btn-login discord" onClick={() => signIn("discord")}>
+              Sign in with Discord
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
