01f49e6678dc7bc984ffff621ba0761d63ea74e6
Marko Malec <CroPsychooo@yahoo.com>
2023-09-06 20:04:36 +0200

    Hero section added spline 3d canvas

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useParallaxEffect } from "~/hooks/useMousePosition";
 import Link from "next/link";
-// import Spline from "@splinetool/react-spline";
+import Spline from "@splinetool/react-spline";
 
 const Hero = () => {
   return (
     <div className="hero-wrapper">
-        {/* <Spline scene="https://prod.spline.design/I2f9okEvGcSA0V1U/scene.splinecode" /> */}
-        <div className="hero-inner">
+      <div className="3d">
+        <Spline scene="https://draft.spline.design/AiO5KRYqrs4vQVeF/scene.splinecode" />
+      </div>
+      <div className="hero-inner">
         <div className="particle square" style={useParallaxEffect(60)} />
         <div className="particle square small" style={useParallaxEffect(50)} />
         <div className="particle line" style={useParallaxEffect(90)} />
@@ -29,6 +31,7 @@ const Hero = () => {
             Learn more
           </Link>
         </div>
+        <div className="hero-photo" />
       </div>
     </div>
   );
