77591e48891c0d37e69ac829e6b0e973facc4d5b
Marko Malec <CroPsychooo@yahoo.com>
2023-09-12 18:23:27 +0200

    Added an OG image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Transition from "@components/Transition/Transition";
 import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
+import Head from "next/head";
 
 const queryClient = new QueryClient();
 
@@ -35,6 +36,12 @@ const MyApp: React.FC<AppProps<{ session: Session | null }>> = ({
   return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
+        <Head>
+          <meta
+            property="og:image"
+            content="https://utfs.io/f/ac32a312-1b8a-42db-a01c-369ed91646dc_IMG_20221001_152017_204.jpg"
+          />
+        </Head>
         <AnimatePresence mode="wait">
           <motion.div key={routerino.route} className="motion_transition_div">
             <Transition />
